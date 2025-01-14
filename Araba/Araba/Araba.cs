78e67bf631ee539a2be41a0b241bc6b94717f7c5
cagatay246 <cagataykilgi7@gmail.com>
2025-01-14 12:13:32 +0300

    1

---
+++ Araba/Araba/Araba.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+class Araba
+{
+    // Özellikler
+    public string Marka { get; set; }
+    public string Model { get; set; }
+    public string Renk { get; set; }
+
+   
+    private int _kapıSayısı;
+
+    public int KapıSayısı
+    {
+        get { return _kapıSayısı; }
+        set
+        {
+            
+            if (value == 2 || value == 4)
+            {
+                _kapıSayısı = value;
+            }
+            else
+            {
+                Console.WriteLine("Uyarı: Kapı sayısı sadece 2 veya 4 olabilir. Kapı sayısı -1 olarak atanıyor.");
+                _kapıSayısı = -1;
+            }
+        }
+    }
+
+    // Constructor
+    public Araba(string marka, string model, string renk, int kapıSayısı)
+    {
+        Marka = marka;
+        Model = model;
+        Renk = renk;
+        KapıSayısı = kapıSayısı;  
+    }
+
+    
+    public void BilgileriYazdir()
+    {
+        Console.WriteLine($"Marka: {Marka}, Model: {Model}, Renk: {Renk}, Kapı Sayısı: {KapıSayısı}");
+    }
+}
---
+++ Araba/Araba/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    static void Main()
+    {
+        Araba araba1 = new Araba("Toyota", "Corolla", "Beyaz", 4);
+        araba1.BilgileriYazdir();
+
+        Araba araba2 = new Araba("BMW", "320i", "Mavi", 3);
+        araba2.BilgileriYazdir();
+
+        Araba araba3 = new Araba("Audi", "A4", "Siyah", 2);
+        araba3.BilgileriYazdir();
+    }
+}
